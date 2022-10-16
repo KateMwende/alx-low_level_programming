c54b3345e4b49cba2992e61b02d2d4e2f5b77be9
KateMwende <kmwende419@gmail.com>
2022-10-16 14:23:25 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int numR;
+
+	for (numR = '0'; numR <= '9'; numR++)
+		putchar(numR);
+
+	putchar('\n');
+
+	return (0);
+
+}
