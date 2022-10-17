b6a1f866b467ad4050fe7c84a1cd1a64becbd841
KateMwende <kmwende419@gmail.com>
2022-10-17 12:47:45 +0000

    Combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n, p;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (p = n + 1; p <= 9; p++)
+		{
+			putchar((n % 10) + '0');
+			putchar((p % 10) + '0');
+
+			if (n == 8 && p == 9)
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
