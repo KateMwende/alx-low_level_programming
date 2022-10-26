600d97fdf73c29a02bd48319d76b5f850f2e09fd
KateMwende <kmwende419@gmail.com>
2022-10-26 09:39:08 +0000

    gitignore

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* rev_string - Reverse a string
+* @s: Pointer to the string
+* Return: void
+*/
+void rev_string(char *s)
+{
+	int i,j;
+	int a = 0;
+
+	while (*(s + a) != '\0')
+	{
+		s++;
+	}
+	while (
+}
