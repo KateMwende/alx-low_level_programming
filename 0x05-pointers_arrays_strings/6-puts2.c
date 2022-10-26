c68d5b1a9e618d6d4999584bd76942e633b063a6
KateMwende <kmwende419@gmail.com>
2022-10-26 11:22:07 +0000

    Every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i++)
-	if ((i % 2) == 0)
-		_putchar(*str + i);
-
+	while (str[i] != '\0')
+	{	
+		if ((i % 2) == 0)
+		_putchar(*(str + i));
+		i++;
+	}
 	_putchar('\n');
 }
