0cd09896fdc2cbec41c2371f99576aafb0f49ad3
KateMwende <kmwende419@gmail.com>
2022-11-02 00:12:38 +0000

    Remove null byte comparison

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; needle[j]; j++)
 		{
 			if (needle[j] == haystack[i])
 				return (needle);
