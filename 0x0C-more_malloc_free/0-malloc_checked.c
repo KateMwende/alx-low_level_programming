7867fad108e4944f5fb7933277549f25b0ddb8a2
KateMwende <kmwende419@gmail.com>
2022-11-11 15:50:19 +0000

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked- check if malloc terminates with status value 98
+* @b: size
+*Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *check;
+
+	check = malloc(b);
+	if (check == NULL)
+		exit(98);
+
+	return (check);
+}
