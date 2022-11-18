5f1a672183775e08461ee2548a8ebc40d0dc2c0b
KateMwende <kmwende419@gmail.com>
2022-11-18 14:16:06 +0000

    Separator conditional

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 	unsigned int p = 0, i;
 
-	if (separator == NULL)
-		return;
-
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
 		p = va_arg(nums, int);
 
-		printf("%d%s", p, separator);
+		printf("%d", p);
+		if (separator != NULL && i < n - 1)
+		printf("%s", separator);
 	}
 	va_end(nums);
 	printf("\n");
