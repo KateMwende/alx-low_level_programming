548273ffdf3ef38468af2db4e51d90604f0268cd
KateMwende <kmwende419@gmail.com>
2022-11-30 10:01:01 +0000

    Main.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,9 +26,10 @@ int _strlen(const char *s)
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	list_t *new_node;
 	list_t *last_node = (*head);
 
+	new_node = malloc(sizeof(list_t));
 	new_node->len = _strlen(str);
 	new_node->str = strdup(str);
 	new_node->next = NULL;
