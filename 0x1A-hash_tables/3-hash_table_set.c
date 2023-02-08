4a968426a1b635fca212153f29692a40475cacdd
Katemwende <kmwende419@gmail.com>
2023-02-08 14:44:25 +0000

    Compile

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new_node->value = strdup(value);
-	if (new_nde->value == NULL)
+	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
